c33e41be38b415c4df9ee934a4baad9ee7fb1259
ogzhncengiz@outlook.com <ogzhncengiz@outlook.com>
2022-10-29 18:07:11 +0300

    Add project files.

---
+++ Request.Response.Middleware.Library/Models/RequestResponseContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Request.Response.Middleware.Library.Models
+{
+    public class RequestResponseContext
+    {
+        private readonly HttpContext _context;
+
+        public RequestResponseContext(HttpContext context)
+        {
+            _context = context;
+        }
+
+        public string RequestBody { get; set; }
+        public string ResponseBody { get; set; }
+
+        [JsonIgnore]
+        public TimeSpan ResponseCreationTime { get; set; }
+        public string FormattedCreationTime =>
+            FormattedCreationTime is null
+                ? "00.00.000"
+                : string.Format("{0:mm\\:ss\\.fff}", ResponseCreationTime);
+
+        public Uri Url => BuildUrl();
+
+        public int? RequestLength => RequestBody?.Length;
+        public int? ResponseLength => ResponseBody?.Length;
+
+        internal Uri BuildUrl()
+        {
+            string url = _context.Request.GetDisplayUrl();
+
+            return new Uri(url, UriKind.RelativeOrAbsolute);
+        }
+    }
+}
---
+++ Request.Response.Middleware.Library/Models/RequestResponseOptions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Request.Response.Middleware.Library.Models
+{
+    public class RequestResponseOptions
+    {
+        internal Func<RequestResponseContext,Task> RequestResponseHandler { get; set; }
+        internal ILoggerFactory LoggerFactory;
+        internal LoggingOptions LoggingOptions;
+
+        public void UseHandler(Func<RequestResponseContext, Task> requestResponseHandler)
+        {
+            RequestResponseHandler = requestResponseHandler;
+        }
+
+        public void UseLogger(ILoggerFactory loggerFavtory, Action<LoggingOptions> loggingAction)
+        {
+            LoggingOptions = new LoggingOptions();
+            loggingAction(LoggingOptions);
+
+            LoggerFactory = loggerFavtory;
+        }
+    }
+}
